422e1220f30cdad4b44d315106559a80b4201998
NoNoNo1100 <913565633@qq.com>
2017-09-22 10:45:01 +0800

    完成启用注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -9,6 +9,31 @@ define(['jquery','template'],function($,template){
         success:function(data){
             var html=template('teacherTpl',{list:data.result});
             $("#teacherInfo").html(html);
+
+            $('.eod').click(function(){
+                var that=this;
+                var td=$(this).closest('td');
+                var tcid=td.attr('data-tcId');
+                var  status=td.attr('data-status');
+                $.ajax({
+                    type:'post',
+                    url:'/api/teacher/handle',
+                    data:{tc_id:tcid,tc_status:status},
+                    dataType:'json',
+                    success:function(data){
+                      if(data.code==200){
+                          console.log(data);
+                          td.attr('data-status',data.result.tc_status);
+                          if(data.result.tc_status==0){
+                              $(that).text('注销');
+                          }else{
+                              $(that).text('启用');
+                          }
+                      }
+                    }
+                })
+
+            })
         }
     })
 });
