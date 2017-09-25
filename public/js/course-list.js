c9e495094fe6ab071e48e82b6384d1859083452a
NoNoNo1100 <913565633@qq.com>
2017-09-25 21:02:33 +0800

    完成课程列表展示

--- public/js/course-list.js
+++ public/js/course-list.js
@@ -3,4 +3,14 @@
  */
 define(['jquery','template','util'],function($,template,util){
 util.setMenu(location.pathname);
+    $.ajax({
+        type:'get',
+        url:'/api/course',
+        dataType:'json',
+        success:function(data){
+          var html=template('courseTpl',{list:data.result});
+            $("#courseInfo").html(html);
+        }
+
+    })
 })
