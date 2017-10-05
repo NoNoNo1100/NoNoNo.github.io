104f5bb75add01492a57cc0fa983d8296eb37909
NoNoNo1100 <913565633@qq.com>
2017-10-05 15:20:59 +0800

    完成课时添加和编辑的弹窗共享

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/10/5.
  */
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','bootstrap'],function($,template,util){
     util.setMenu('/course/add');
     var csId=util.qs('cs_id');
     $.ajax({
@@ -12,6 +12,28 @@ define(['jquery','template','util'],function($,template,util){
         success:function(data){
             var html=template('lessonTpl',data.result);
             $('#lessonInfo').html(html);
+          /*  绑定添加课时事件*/
+            $('#addLesson').click(function(){
+                var html=template('modalTpl',{operate:'添加课时'});
+                $('#modalInfo').html(html);
+                $('#chapterModal').modal();
+            });
+         /*   编辑课时*/
+            $('.editBtn').click(function(){
+                var ctId=$(this).attr('data-ctId');
+                $.ajax({
+                    type:'get',
+                    url:'/api/course/chapter/edit',
+                    data:{ct_id:ctId},
+                    dataType:'json',
+                    success:function(data){
+                        data.result.operate='编辑课时';
+                       var html=template('modalTpl',data.result);
+                        $('#modalInfo').html(html);
+                        $('#chapterModal').modal();
+                    }
+                })
+            })
         }
     })
 })
